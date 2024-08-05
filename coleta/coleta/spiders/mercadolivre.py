f6fbf7da2ec4fb3a48e4e9f16e01b308b53251a2
Pedro Henrique <pedroh.brandao26@gmail.com>
2024-08-04 23:11:59 -0300

    atualizar

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -7,4 +7,12 @@ class MercadolivreSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/calcados"]
 
     def parse(self, response):
+        products = response.css('div.ui-search-result__content')
+
+        for prouct in products:
+            
+            yield{
+            'brand':products.css('span.ui-search-item__brand-discoverability ui-search-item__group__element::text').get()
+            }
+
         pass
